551ce59f03ee34a172bd11bf74358ca543537c33
Mozibix <opuzmoses@gmail.com>
2023-11-08 05:52:35 +0100

    chores: updated _document component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+export default function MyDocument() {
   return (
-    <Html lang="en">
-      <Head />
+    <html lang="en">
+      <Head>
+        <title>Demo Project</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-    </Html>
-  )
+    </html>
+  );
 }
