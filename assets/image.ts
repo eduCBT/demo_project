d115e6cf9c7e0da85786836913092e99f350ee44
Mozibix <opuzmoses@gmail.com>
2023-11-08 05:52:17 +0100

    chores:  updated image.ts imports

---
+++ assets/image.ts
@@ -0,0 +1,9 @@
+/* IMAGE IMPORTS STORED */
+
+export { default as hamburger } from "./hamburger.svg";
+export { default as hero_img } from "./hero_img.svg";
+export { default as linkedin_icon } from "./linkedin_icon.svg";
+export { default as medium_icon } from "./medium_icon.svg";
+export { default as twitter_icon } from "./twitter_icon.svg";
+export { default as post_img } from "./post_img.svg";
+export { default as post_img2 } from "./post_img2.svg";
